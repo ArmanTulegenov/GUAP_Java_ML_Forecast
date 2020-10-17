e1f18a7220e56dc5a5de41903e7dafd00a1a6c03
Arman Tulegenov <arman.tulegenov@gmail.com>
2020-10-17 11:09:11 +0300

    lecture #2 commit #2

--- src/ru/guap/Main.java
+++ src/ru/guap/Main.java
@@ -6,13 +6,38 @@ import ru.guap.model.PanelMadeBuilding;
 public class Main {
 
     public static void main(String[] args) {
-        Building building = new Building("Хрущевка");
+        Building building = new Building("Хрущевка", "");
         System.out.println(building.getType());
 
-        Building building1 = new Building("Новый комплекс на Лиговке");
-        System.out.println(building1.getType());
+        Building building1 = new Building("Новый комплекс на Лиговке", "");
+        System.out.println(building1.toString());
+
+        Building building1_1 = new Building("Новый комплекс на Лиговке", "");
+        System.out.println(building1_1.toString());
 
         Building building2 = new PanelMadeBuilding();
         System.out.println(building2.getType());
+
+        Object object1 = building;
+        System.out.println(object1);
+
+        System.out.println(building == object1);
+        System.out.println(building1 == building2);
+        System.out.println(building1.equals(building2));
+
+        System.out.println("Result ----->");
+        System.out.println(building1 == building1_1);
+        System.out.println(building1.equals(building1_1));
+
+        Boolean valueIs = Boolean.TRUE;
+        boolean literalValue = true;
+
+        Integer valueOfInteger = 1000;
+        int valueOfInt = 1000;
+
+        System.out.println(valueOfInteger == valueOfInt);
+
+
+
     }
 }
--- src/ru/guap/model/Building.java
+++ src/ru/guap/model/Building.java
@@ -1,10 +1,18 @@
 package ru.guap.model;
 
+import java.util.Objects;
+
+/*
+
+Строения методы и свойства, которые будут наследовать все типы строений
+ */
 public class Building {
     private String type;
+    private String address;
 
-    public Building(String type) {
+    public Building(String type, String address) {
         this.type = type;
+        this.address = address;
     }
 
     public String getType() {
@@ -13,5 +21,32 @@ public class Building {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Тип здания: %s", type);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Building building = (Building) o;
+        return Objects.equals(type, building.type) &&
+                Objects.equals(address, building.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, address);
     }
 }
