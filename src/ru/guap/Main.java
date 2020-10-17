f75737bb573d964128e434aad8c63dd245d5ba6b
Arman Tulegenov <arman.tulegenov@gmail.com>
2020-10-17 10:03:01 +0300

    lecture #2 commit #1

--- src/ru/guap/Main.java
+++ src/ru/guap/Main.java
@@ -9,7 +9,10 @@ public class Main {
         Building building = new Building("Хрущевка");
         System.out.println(building.getType());
 
-        Building building1 = new PanelMadeBuilding();
+        Building building1 = new Building("Новый комплекс на Лиговке");
         System.out.println(building1.getType());
+
+        Building building2 = new PanelMadeBuilding();
+        System.out.println(building2.getType());
     }
 }
