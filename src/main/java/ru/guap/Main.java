d5cb82e7c07d84903e6e7bf6dc00311a90631b1a
Arman Tulegenov <arman.tulegenov@gmail.com>
2020-10-24 15:36:10 +0300

    add tensorflow bytedeco

--- src/main/java/ru/guap/Main.java
+++ src/main/java/ru/guap/Main.java
@@ -1,43 +1,20 @@
 package ru.guap;
 
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
 import ru.guap.model.Building;
 import ru.guap.model.PanelMadeBuilding;
 
 public class Main {
 
     public static void main(String[] args) {
-        Building building = new Building("Хрущевка", "");
-        System.out.println(building.getType());
+        try (Graph g = new Graph()) {
+            final String value = "Hello from " + TensorFlow.version();
 
-        Building building1 = new Building("Новый комплекс на Лиговке", "");
-        System.out.println(building1.toString());
-
-        Building building1_1 = new Building("Новый комплекс на Лиговке", "");
-        System.out.println(building1_1.toString());
-
-        Building building2 = new PanelMadeBuilding();
-        System.out.println(building2.getType());
-
-        Object object1 = building;
-        System.out.println(object1);
-
-        System.out.println(building == object1);
-        System.out.println(building1 == building2);
-        System.out.println(building1.equals(building2));
-
-        System.out.println("Result ----->");
-        System.out.println(building1 == building1_1);
-        System.out.println(building1.equals(building1_1));
-
-        Boolean valueIs = Boolean.TRUE;
-        boolean literalValue = true;
-
-        Integer valueOfInteger = 1000;
-        int valueOfInt = 1000;
-
-        System.out.println(valueOfInteger == valueOfInt);
-
-
+            System.out.println(value);
+        }
 
     }
 }
